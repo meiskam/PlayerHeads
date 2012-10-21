1b626b81cb700fff68eb1448c5e7678cc621662e
meiskam <meiskam@gmail.com>
2012-10-21 05:03:50 -0400

    merged listener to main, added commands, configuration, and drop chance

--- src/org/shininet/bukkit/PlayerHeads/PlayerHeads.java
+++ src/org/shininet/bukkit/PlayerHeads/PlayerHeads.java
@@ -1,41 +1,143 @@
 package org.shininet.bukkit.PlayerHeads;
 
+import java.util.Random;
+
 import net.minecraft.server.NBTTagCompound;
 
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.craftbukkit.inventory.CraftItemStack;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class PlayerHeads extends JavaPlugin {
+public final class PlayerHeads extends JavaPlugin implements Listener {
 	
-	public final PlayerDeathListener playerDeathListener = new PlayerDeathListener();
+	private Random generator = new Random();
 	
 	@Override
 	public void onEnable(){
-		//getLogger().info(getDescription().getFullName()+" enabled");
-		getServer().getPluginManager().registerEvents(playerDeathListener, this);
+		getServer().getPluginManager().registerEvents((Listener)this, this);
 	}
  
 	@Override
 	public void onDisable() {
-		//getLogger().info(getDescription().getFullName()+" disabled");
-		EntityDeathEvent.getHandlerList().unregister(playerDeathListener);
+		EntityDeathEvent.getHandlerList().unregister((Listener)this);
 	}
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
 		if (cmd.getName().equalsIgnoreCase("PlayerHeads")) {
-			sender.sendMessage("I see that you typed /"+label+", but this command currently does nothing.");
+			if (args.length == 0) {
+				sender.sendMessage("["+label+"] Subcommands: config, spawn");
+			} else {
+				if (args[0].equalsIgnoreCase("config")) {
+					if (args.length == 1) {
+						sender.sendMessage("["+label+":config] Subcommands: get, set, reload");
+					} else {
+						if (args[1].equalsIgnoreCase("get") || args[1].equalsIgnoreCase("view")) {
+							if (((Player)sender).hasPermission("playerheads.config.get")) {
+								if (args.length == 2) {
+									sender.sendMessage("["+label+":config:get] Config variables: DropRate, PKOnly");
+								} else if (args.length == 3) {
+									sender.sendMessage("["+label+":config:get] "+args[2].toLowerCase()+": "+getConfig().get(args[2].toLowerCase()));
+								} else {
+									sender.sendMessage("["+label+":config:get] Too many params!");
+								}
+							} else {
+								sender.sendMessage("["+label+":config:get] You don't have permission to use that command");
+							}
+						} else if (args[1].equalsIgnoreCase("set")) {
+							if (((Player)sender).hasPermission("playerheads.config.set")) {
+								if (args.length == 2 || args.length == 3) {
+									sender.sendMessage("["+label+":config:set] Config variables: DropRate, PKOnly");
+								} else if (args.length == 4) {
+									getConfig().set(args[2].toLowerCase(), args[3]);
+									saveConfig();
+								} else {
+									sender.sendMessage("["+label+":config:set] Too many params!");
+								}
+							} else {
+								sender.sendMessage("["+label+":config:set] You don't have permission to use that command");
+							}
+/*						} else if (args[1].equalsIgnoreCase("save")) {
+							if (((Player)sender).hasPermission("playerheads.config.set")) {
+								saveConfig();
+							} else {
+								sender.sendMessage("["+label+":config:save] You don't have permission to use that command");
+							}
+*/						} else if (args[1].equalsIgnoreCase("reload")) {
+							if (((Player)sender).hasPermission("playerheads.config.set")) {
+								reloadConfig();
+							} else {
+								sender.sendMessage("["+label+":config:reload] You don't have permission to use that command");
+							}
+						} else {
+							sender.sendMessage("["+label+":config:??] Invalid subcommand");
+						}
+					}
+				}
+				else if (args[0].equalsIgnoreCase("spawn")) {
+					if (!(sender instanceof Player)) {
+						sender.sendMessage("["+label+":spawn] Sorry console, heads are for players!");
+					}
+					else {
+						if (args.length == 1) {
+							//spawn them their head
+							if (((Player)sender).hasPermission("playerheads.spawn.own")) {
+								if (((Player)sender).getInventory().addItem(getHead(((Player)sender).getName())).isEmpty()) {
+									sender.sendMessage("["+label+":spawn] Spawned you "+((Player)sender).getName()+"'s head");
+								} else {
+									sender.sendMessage("["+label+":spawn] Well I can't very well give you an item if your inventory is full");
+								}
+							} else {
+								sender.sendMessage("["+label+":spawn] You don't have permission to use that command");
+							}
+						} else if (args.length == 2) {
+							//spawn them args[1]'s head
+							if (((Player)sender).hasPermission("playerheads.spawn")) {
+								if (((Player)sender).getInventory().addItem(getHead(args[1])).isEmpty()) {
+									sender.sendMessage("["+label+":spawn] Spawned you "+args[1]+"'s head");
+								} else {
+									sender.sendMessage("["+label+":spawn] Well I can't very well give you an item if your inventory is full");
+								}
+							} else {
+								sender.sendMessage("["+label+":spawn] You don't have permission to use that command");
+							}
+						} else {
+							sender.sendMessage("["+label+":spawn] Too many params!");
+						}
+					}
+				} else if (args[0].equalsIgnoreCase("somethingelse")) {
+					sender.sendMessage("["+label+":??] moo");
+				} else {
+					sender.sendMessage("["+label+":??] Invalid subcommand");
+				}
+			}
 			return true;
 		}
 		return false; 
 	}
+	
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void onEntityDeath(EntityDeathEvent event) {
+		if (event.getEntityType() == EntityType.PLAYER) {
+			//((Player)(event.getEntity())).sendMessage("Hehe, you died");
+			if ((!(getConfig().getBoolean("pkonly", true)) // if pkonly's off, continue, don't check next if line
+			 || (getConfig().getBoolean("pkonly", true) && (event.getEntity().getKiller() instanceof Player))) // if pkonly's on AND killer is player, continue 
+			 && (generator.nextDouble() <= getConfig().getDouble("droprate", 0.05))) { // check if it should drop via droprate
+				event.getDrops().add(getHead(((Player)(event.getEntity())).getName())); // drop the precious player head
+			}
+		}
+	}
 
-	public static CraftItemStack getHead(String playername) {
-		CraftItemStack head = new CraftItemStack(Material.getMaterial(276),1,(short)3);
+	public CraftItemStack getHead(String playername) {
+		CraftItemStack head = new CraftItemStack(Material.getMaterial(276),1,(short)3); //TODO: Skull is id 144
 		NBTTagCompound headNBT = new NBTTagCompound();
 		headNBT.setString("SkullOwner", playername);
 		head.getHandle().tag = headNBT;

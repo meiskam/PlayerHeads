4c3d7f411ba808ad48cbb9ddd20cb6f67dbf3966
Daniel <danielyourbest@outlook.com>
2013-09-09 17:09:15 +0300

    Add Magma Cube and Wither: increase priority of...
    MHF since those are official heads.

--- src/main/java/org/shininet/bukkit/playerheads/CustomSkullType.java
+++ src/main/java/org/shininet/bukkit/playerheads/CustomSkullType.java
@@ -12,27 +12,29 @@ import java.util.HashMap;
 
 public enum CustomSkullType {
 
-    SPIDER("Kelevra_V", "MHF_Spider"), // Thanks SethBling
-    ENDERMAN("Violit", "MHF_Enderman"), // Thanks SethBling
-    BLAZE("Blaze_Head", "MHF_Blaze"), // Thanks SethBling
+    SPIDER("MHF_Spider", "Kelevra_V"), // Thanks SethBling
+    ENDERMAN("MHF_Enderman", "Violit", "Troax"), // Thanks SethBling
+    BLAZE("MHF_Blaze", "Blaze_Head"), // Thanks SethBling
     HORSE("gavertoso"), // Thanks Glompalici0us
-    SQUID("squidette8", "MHF_Squid"), // Thanks SethBling
+    SQUID("MHF_Squid", "squidette8"), // Thanks SethBling
     SILVERFISH("AlexVMiner"), // Thanks SethBling
     ENDER_DRAGON("KingEndermen", "KingEnderman"), // Thanks SethBling
-    SLIME("HappyHappyMan", "Ex_PS3Zocker", "MHF_Slime"), // Thanks SethBling
+    SLIME("MHF_Slime", "HappyHappyMan", "Ex_PS3Zocker", "BlackJake"), // Thanks SethBling
     IRON_GOLEM("zippie007"), // Thanks SethBling
-    MUSHROOM_COW("Mooshroom_Stew", "MHF_MushroomCow"), // Thanks SethBling
+    MUSHROOM_COW("MHF_MushroomCow", "Mooshroom_Stew", "BaileeBitch"), // Thanks SethBling
     BAT("bozzobrain", "coolwhip101"), // Thanks incraftion.com
-    PIG_ZOMBIE("ManBearPigZombie", "scraftbrothers5", "MHF_PigZombie"), // Thanks cnaude of TrophyHeads
-    SNOWMAN("Koebasti", "scraftbrothers2"), // Thanks MrLeikermoser
-    GHAST("_QuBra_", "blaiden", "MHF_Ghast"), // Thanks MrLeikermoser
+    PIG_ZOMBIE("MHF_PigZombie", "ManBearPigZombie", "scraftbrothers5"), // Thanks cnaude of TrophyHeads
+    SNOWMAN("Koebasti", "scraftbrothers2", "GLaDOS"), // Thanks MrLeikermoser
+    GHAST("MHF_Ghast", "_QuBra_", "blaiden"), // Thanks MrLeikermoser
     PIG("XlexerX", "scrafbrothers7"), // Thanks XlexerX
-    VILLAGER("Kuvase", "Villager", "scraftbrothers9", "MHF_Villager"), // Thanks XlexerX
-    SHEEP("SGT_KICYORASS", "Eagle_Peak", "MHF_Sheep"), // Thanks cowboys2317
-    COW("VerifiedBernard", "CarlosTheCow", "MHF_Cow"), // Thanks Jknies
-    CHICKEN("scraftbrothers1", "MHF_Chicken"), // Thanks SuperCraftBrothers.com
+    VILLAGER("MHF_Villager", "Kuvase", "Villager", "scraftbrothers9"), // Thanks XlexerX
+    SHEEP("MHF_Sheep", "SGT_KICYORASS", "Eagle_Peak", "HyperBeam567"), // Thanks cowboys2317
+    COW("MHF_Cow", "VerifiedBernard", "CarlosTheCow", "Seann_G"), // Thanks Jknies
+    CHICKEN("MHF_Chicken", "scraftbrothers1", "Gafloff"), // Thanks SuperCraftBrothers.com
     OCELOT("scraftbrothers3"), // Thanks SuperCraftBrothers.com
-    WITCH("scrafbrothers4"); // Thanks SuperCraftBrothers.com
+    WITCH("scrafbrothers4"), // Thanks SuperCraftBrothers.com
+    MAGMA_CUBE("MHF_LavaSlime", "brinkley26"), // Thanks Marc_IRL of Mojang
+    WITHER("Mad_Miner63") // Thanks Mad_Miner63
 
     private final String owner;
     private static class Holder {

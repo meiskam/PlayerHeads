4ea1387a06ee8a1bbc278863ebeb1c0f42c7e1ec
crashdemons <crashenator+gh@gmail.com>
2018-09-08 17:15:38 -0500

    Compact command messages to make them more readable. Fix lang *_SPACE entries to actually contain spaces... (why didn't they?).

--- src/main/java/org/shininet/bukkit/playerheads/PlayerHeadsCommandExecutor.java
+++ src/main/java/org/shininet/bukkit/playerheads/PlayerHeadsCommandExecutor.java
@@ -27,6 +27,10 @@ class PlayerHeadsCommandExecutor implements CommandExecutor, TabCompleter {
     public PlayerHeadsCommandExecutor(PlayerHeads plugin) {
         this.plugin = plugin;
     }
+    
+    private void formatMsg(CommandSender sender, String commandscope, String message, String... replacements){
+        Tools.formatMsg(sender, Lang.BRACKET_LEFT + commandscope + Lang.BRACKET_RIGHT + Lang.SPACE + message, replacements);
+    }
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
@@ -48,38 +52,53 @@ class PlayerHeadsCommandExecutor implements CommandExecutor, TabCompleter {
         }
         */
         
+        String scope=label;
+        
         if (args.length == 0) {
-            Tools.formatMsg(sender, Lang.BRACKET_LEFT + label + Lang.BRACKET_RIGHT + Lang.SPACE + Lang.SUBCOMMANDS + Lang.COLON_SPACE + Lang.CMD_CONFIG + Lang.COMMA_SPACE + Lang.CMD_SPAWN + Lang.COMMA_SPACE + Lang.CMD_RENAME);
+            formatMsg(sender,scope,Lang.SUBCOMMANDS + Lang.COLON_SPACE + 
+                    Lang.CMD_CONFIG + Lang.COMMA_SPACE + 
+                    Lang.CMD_SPAWN + Lang.COMMA_SPACE + 
+                    Lang.CMD_RENAME
+            );
+            //[ph] Subcommands: config, spawn, rename
             return true;
         }
         if (args[0].equalsIgnoreCase(Tools.formatStrip(Lang.CMD_CONFIG))) {
+            scope+=Lang.COLON + Lang.CMD_CONFIG;
             if (args.length == 1) {
-                Tools.formatMsg(sender, Lang.BRACKET_LEFT + label + Lang.COLON + Lang.CMD_CONFIG + Lang.BRACKET_RIGHT + Lang.SPACE + Lang.SUBCOMMANDS + Lang.COLON_SPACE + Lang.CMD_GET + Lang.COMMA_SPACE + Lang.CMD_SET + Lang.COMMA_SPACE + Lang.CMD_RELOAD);
+                formatMsg(sender, scope, Lang.SUBCOMMANDS + Lang.COLON_SPACE + 
+                        Lang.CMD_GET + Lang.COMMA_SPACE + 
+                        Lang.CMD_SET + Lang.COMMA_SPACE + 
+                        Lang.CMD_RELOAD
+                );
+                //[ph:config] Subcommands: get, set, reload
                 return true;
             }
             if (args[1].equalsIgnoreCase(Tools.formatStrip(Lang.CMD_GET))) {
+                scope+=Lang.COLON + Lang.CMD_GET;
                 if (!sender.hasPermission("playerheads.config.get")) {
-                    Tools.formatMsg(sender, Lang.BRACKET_LEFT + label + Lang.COLON + Lang.CMD_CONFIG + Lang.COLON + Lang.CMD_GET + Lang.BRACKET_RIGHT + Lang.SPACE + Lang.ERROR_PERMISSION);
+                    formatMsg(sender, scope, Lang.ERROR_PERMISSION);
                     return true;
                 }
                 if (args.length == 3) {
                     String key = args[2].toLowerCase();
-                    Tools.formatMsg(sender, Lang.BRACKET_LEFT + label + Lang.COLON + Lang.CMD_CONFIG + Lang.COLON + Lang.CMD_GET + Lang.BRACKET_RIGHT + Lang.SPACE + key + Lang.COLON_SPACE + plugin.configFile.get(key));
+                    formatMsg(sender, scope, key + Lang.COLON_SPACE + plugin.configFile.get(key));
                 } else {
-                    Tools.formatMsg(sender, Lang.BRACKET_LEFT + label + Lang.COLON + Lang.CMD_CONFIG + Lang.COLON + Lang.CMD_GET + Lang.BRACKET_RIGHT + Lang.SPACE + Lang.SYNTAX + Lang.COLON_SPACE + label + Lang.SPACE + Lang.CMD_CONFIG + Lang.SPACE + Lang.CMD_GET + Lang.SPACE + Lang.OPT_VARIABLE_REQUIRED);
-                    Tools.formatMsg(sender, Lang.BRACKET_LEFT + label + Lang.COLON + Lang.CMD_CONFIG + Lang.COLON + Lang.CMD_GET + Lang.BRACKET_RIGHT + Lang.SPACE + Lang.CONFIG_VARIABLES + Lang.COLON_SPACE + Config.configKeysString);
+                    formatMsg(sender, scope, Lang.SYNTAX + Lang.COLON_SPACE + scope + Lang.SPACE + Lang.OPT_VARIABLE_REQUIRED);//Syntax: ph config get <whatever>
+                    formatMsg(sender, scope, Lang.CONFIG_VARIABLES + Lang.COLON_SPACE + Config.configKeysString);//Config variables: x, y, z
                 }
                 return true;
             } else if (args[1].equalsIgnoreCase(Tools.formatStrip(Lang.CMD_SET))) {
+                scope+=Lang.COLON + Lang.CMD_SET;
                 if (!sender.hasPermission("playerheads.config.set")) {
-                    Tools.formatMsg(sender, Lang.BRACKET_LEFT + label + Lang.COLON + Lang.CMD_CONFIG + Lang.COLON + Lang.CMD_SET + Lang.BRACKET_RIGHT + Lang.SPACE + Lang.ERROR_PERMISSION);
+                    formatMsg(sender, scope, Lang.ERROR_PERMISSION);
                     return true;
                 }
                 if (args.length == 3) {
                     String key = args[2].toLowerCase();
                     plugin.configFile.set(key, null);
                     plugin.saveConfig();
-                    Tools.formatMsg(sender, Lang.BRACKET_LEFT + label + Lang.COLON + Lang.CMD_CONFIG + Lang.COLON + Lang.CMD_SET + Lang.BRACKET_RIGHT + Lang.SPACE + key + Lang.COLON_SPACE + plugin.configFile.get(key));
+                    formatMsg(sender, scope, key + Lang.COLON_SPACE + plugin.configFile.get(key));
                     return true;
                 } else if (args.length == 4) {
                     String key = args[2].toLowerCase();
@@ -101,14 +120,14 @@ class PlayerHeadsCommandExecutor implements CommandExecutor, TabCompleter {
                                     try {
                                         plugin.configFile.set(key, Double.parseDouble(value));
                                     } catch (NumberFormatException e) {
-                                        Tools.formatMsg(sender, Lang.BRACKET_LEFT + label + Lang.COLON + Lang.CMD_CONFIG + Lang.COLON + Lang.CMD_SET + Lang.BRACKET_RIGHT + Lang.SPACE + Lang.ERROR_NUMBERCONVERT, value);
+                                        formatMsg(sender, scope, Lang.ERROR_NUMBERCONVERT, value);
                                     }
                                     break;
                                 case INT:
                                     try {
                                         plugin.configFile.set(key, Integer.parseInt(value));
                                     } catch (NumberFormatException e) {
-                                        Tools.formatMsg(sender, Lang.BRACKET_LEFT + label + Lang.COLON + Lang.CMD_CONFIG + Lang.COLON + Lang.CMD_SET + Lang.BRACKET_RIGHT + Lang.SPACE + Lang.ERROR_NUMBERCONVERT, value);
+                                        formatMsg(sender, scope, Lang.ERROR_NUMBERCONVERT, value);
                                     }
                                     break;
                                 default:
@@ -122,28 +141,31 @@ class PlayerHeadsCommandExecutor implements CommandExecutor, TabCompleter {
                         plugin.configFile.set(key, value);
                     }
                     plugin.saveConfig();
-                    Tools.formatMsg(sender, Lang.BRACKET_LEFT + label + Lang.COLON + Lang.CMD_CONFIG + Lang.COLON + Lang.CMD_SET + Lang.BRACKET_RIGHT + Lang.SPACE + key + Lang.COLON_SPACE + plugin.configFile.get(key));
+                    formatMsg(sender, scope, key + Lang.COLON_SPACE + plugin.configFile.get(key));
                     return true;
                 } else {
-                    Tools.formatMsg(sender, Lang.BRACKET_LEFT + label + Lang.COLON + Lang.CMD_CONFIG + Lang.COLON + Lang.CMD_SET + Lang.BRACKET_RIGHT + Lang.SPACE + Lang.SYNTAX + Lang.COLON_SPACE + label + Lang.SPACE + Lang.CMD_CONFIG + Lang.SPACE + Lang.CMD_SET + Lang.SPACE + Lang.OPT_VARIABLE_REQUIRED + Lang.SPACE + Lang.OPT_VALUE_OPTIONAL);
-                    Tools.formatMsg(sender, Lang.BRACKET_LEFT + label + Lang.COLON + Lang.CMD_CONFIG + Lang.COLON + Lang.CMD_SET + Lang.BRACKET_RIGHT + Lang.SPACE + Lang.CONFIG_VARIABLES + Lang.COLON_SPACE + Config.configKeysString);
+                    formatMsg(sender, scope, Lang.SYNTAX + Lang.COLON_SPACE + scope + Lang.SPACE + Lang.OPT_VARIABLE_REQUIRED + Lang.SPACE + Lang.OPT_VALUE_OPTIONAL);
+                    formatMsg(sender, scope, Lang.CONFIG_VARIABLES + Lang.COLON_SPACE + Config.configKeysString);
                     return true;
                 }
             } else if (args[1].equalsIgnoreCase(Tools.formatStrip(Lang.CMD_RELOAD))) {
+                scope+=Lang.COLON + Lang.CMD_RELOAD;
                 if (!sender.hasPermission("playerheads.config.set")) {
-                    Tools.formatMsg(sender, Lang.BRACKET_LEFT + label + Lang.COLON + Lang.CMD_CONFIG + Lang.COLON + Lang.CMD_RELOAD + Lang.BRACKET_RIGHT + Lang.SPACE + Lang.ERROR_PERMISSION);
+                    formatMsg(sender, scope, Lang.ERROR_PERMISSION);
                     return true;
                 }
                 plugin.reloadConfig();
                 plugin.configFile = plugin.getConfig();
                 Lang.reload();
-                Tools.formatMsg(sender, Lang.BRACKET_LEFT + label + Lang.COLON + Lang.CMD_CONFIG + Lang.COLON + Lang.CMD_RELOAD + Lang.BRACKET_RIGHT + Lang.SPACE + Lang.CONFIG_RELOADED);
+                formatMsg(sender, scope, Lang.CONFIG_RELOADED);
                 return true;
             } else {
-                Tools.formatMsg(sender, Lang.BRACKET_LEFT + label + Lang.COLON + Lang.CMD_CONFIG + Lang.COLON + Lang.CMD_UNKNOWN + Lang.BRACKET_RIGHT + Lang.SPACE + Lang.ERROR_INVALID_SUBCOMMAND);
+                scope+=Lang.COLON + Lang.CMD_UNKNOWN;
+                formatMsg(sender, scope, Lang.ERROR_INVALID_SUBCOMMAND);
                 return true;
             }
         } else if (args[0].equalsIgnoreCase(Tools.formatStrip(Lang.CMD_SPAWN))) {
+            scope+=Lang.COLON + Lang.CMD_SPAWN;
             String skullOwner;
             boolean haspermission;
             Player reciever = null;
@@ -152,7 +174,7 @@ class PlayerHeadsCommandExecutor implements CommandExecutor, TabCompleter {
 
             if (isConsoleSender) {
                 if ((args.length != 3) && (args.length != 4)) {
-                    Tools.formatMsg(sender, Lang.BRACKET_LEFT + label + Lang.COLON + Lang.CMD_SPAWN + Lang.BRACKET_RIGHT + Lang.SPACE + Lang.SYNTAX + Lang.COLON_SPACE + label + Lang.SPACE + Lang.CMD_SPAWN + Lang.SPACE + Lang.OPT_HEADNAME_REQUIRED + Lang.SPACE + Lang.OPT_RECEIVER_REQUIRED + Lang.SPACE + Lang.OPT_AMOUNT_OPTIONAL);
+                    formatMsg(sender, scope, Lang.SYNTAX + Lang.COLON_SPACE + scope + Lang.SPACE + Lang.OPT_HEADNAME_REQUIRED + Lang.SPACE + Lang.OPT_RECEIVER_REQUIRED + Lang.SPACE + Lang.OPT_AMOUNT_OPTIONAL);
                     return true;
                 }
             } else {
@@ -172,7 +194,7 @@ class PlayerHeadsCommandExecutor implements CommandExecutor, TabCompleter {
                     }
                 }
                 if ((reciever = plugin.getServer().getPlayer(args[2])) == null) {
-                    Tools.formatMsg(sender, Lang.BRACKET_LEFT + label + Lang.COLON + Lang.CMD_SPAWN + Lang.BRACKET_RIGHT + Lang.SPACE + Lang.ERROR_NOT_ONLINE, args[2]);
+                    formatMsg(sender, scope, Lang.ERROR_NOT_ONLINE, args[2]);
                     return true;
                 }
                 skullOwner = args[1];
@@ -182,39 +204,40 @@ class PlayerHeadsCommandExecutor implements CommandExecutor, TabCompleter {
                     haspermission = sender.hasPermission("playerheads.spawn.forother");
                 }
             } else {
-                Tools.formatMsg(sender, Lang.BRACKET_LEFT + label + Lang.COLON + Lang.CMD_SPAWN + Lang.BRACKET_RIGHT + Lang.SPACE + Lang.SYNTAX + Lang.COLON_SPACE + label + Lang.SPACE + Lang.CMD_SPAWN + Lang.SPACE + Lang.OPT_HEADNAME_OPTIONAL + Lang.SPACE + Lang.OPT_RECEIVER_OPTIONAL + Lang.SPACE + Lang.OPT_AMOUNT_OPTIONAL);
+                formatMsg(sender, scope, Lang.SYNTAX + Lang.COLON_SPACE + scope + Lang.SPACE + Lang.OPT_HEADNAME_OPTIONAL + Lang.SPACE + Lang.OPT_RECEIVER_OPTIONAL + Lang.SPACE + Lang.OPT_AMOUNT_OPTIONAL);
                 return true;
             }
             if (!haspermission) {
-                Tools.formatMsg(sender, Lang.BRACKET_LEFT + label + Lang.COLON + Lang.CMD_SPAWN + Lang.BRACKET_RIGHT + Lang.SPACE + Lang.ERROR_PERMISSION);
+                formatMsg(sender, scope, Lang.ERROR_PERMISSION);
                 return true;
             }
             if (plugin.configFile.getBoolean("fixcase")) {
                 skullOwner = Tools.fixcase(skullOwner);
             }
             if (Tools.addHead(reciever, skullOwner, quantity)) {
-                Tools.formatMsg(sender, Lang.BRACKET_LEFT + label + Lang.COLON + Lang.CMD_SPAWN + Lang.BRACKET_RIGHT + Lang.SPACE + Lang.SPAWNED_HEAD, skullOwner);
+                formatMsg(sender, scope, Lang.SPAWNED_HEAD, skullOwner);
             } else {
-                Tools.formatMsg(sender, Lang.BRACKET_LEFT + label + Lang.COLON + Lang.CMD_SPAWN + Lang.BRACKET_RIGHT + Lang.SPACE + Lang.ERROR_INV_FULL);
+                formatMsg(sender, scope, Lang.ERROR_INV_FULL);
             }
             return true;
         } else if (args[0].equalsIgnoreCase(Tools.formatStrip(Lang.CMD_RENAME))) {
+            scope+=Lang.COLON + Lang.CMD_RENAME;
             if (!(sender instanceof Player)) {
-                Tools.formatMsg(sender, Lang.BRACKET_LEFT + label + Lang.COLON + Lang.CMD_SPAWN + Lang.BRACKET_RIGHT + Lang.SPACE + Lang.ERROR_CONSOLE_SPAWN);
+                formatMsg(sender, scope, Lang.ERROR_CONSOLE_SPAWN);
                 return true;
             }
             if (!sender.hasPermission("playerheads.rename")) {
-                Tools.formatMsg(sender, Lang.BRACKET_LEFT + label + Lang.COLON + Lang.CMD_RENAME + Lang.BRACKET_RIGHT + Lang.SPACE + Lang.ERROR_PERMISSION);
+                formatMsg(sender, scope, Lang.ERROR_PERMISSION);
                 return true;
             }
             if (!((args.length == 1) || (args.length == 2))) {
-                Tools.formatMsg(sender, Lang.BRACKET_LEFT + label + Lang.COLON + Lang.CMD_RENAME + Lang.BRACKET_RIGHT + Lang.SPACE + Lang.SYNTAX + Lang.COLON_SPACE + label + Lang.SPACE + Lang.CMD_RENAME + Lang.SPACE + Lang.OPT_HEADNAME_OPTIONAL);
+                formatMsg(sender, scope, Lang.SYNTAX + Lang.COLON_SPACE + scope + Lang.SPACE + Lang.OPT_HEADNAME_OPTIONAL);
                 return true;
             }
             ItemStack skullInput = ((Player) sender).getEquipment().getItemInMainHand();
             Material inputType = skullInput.getType();
             if ( TexturedSkullType.get(skullInput.getType())==null ) {
-                Tools.formatMsg(sender, Lang.BRACKET_LEFT + label + Lang.COLON + Lang.CMD_RENAME + Lang.BRACKET_RIGHT + Lang.SPACE + Lang.ERROR_NOT_A_HEAD);
+                formatMsg(sender, scope, Lang.ERROR_NOT_A_HEAD);
                 return true;
             }
             ItemStack skullOutput;
@@ -229,10 +252,11 @@ class PlayerHeadsCommandExecutor implements CommandExecutor, TabCompleter {
             }
             skullOutput.setAmount(skullInput.getAmount());
             ((Player) sender).getEquipment().setItemInMainHand(skullOutput);
-            Tools.formatMsg(sender, Lang.BRACKET_LEFT + label + Lang.COLON + Lang.CMD_RENAME + Lang.BRACKET_RIGHT + Lang.SPACE + Lang.RENAMED_HEAD);
+            formatMsg(sender, scope, Lang.RENAMED_HEAD);
             return true;
         } else {
-            Tools.formatMsg(sender, Lang.BRACKET_LEFT + label + Lang.COLON + Lang.CMD_UNKNOWN + Lang.BRACKET_RIGHT + Lang.SPACE + Lang.ERROR_INVALID_SUBCOMMAND);
+            scope+=Lang.COLON + Lang.CMD_UNKNOWN;
+            formatMsg(sender, scope, Lang.ERROR_INVALID_SUBCOMMAND);
             return true;
         }
     }

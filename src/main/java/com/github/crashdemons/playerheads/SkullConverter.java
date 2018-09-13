5389cd06a8e106e3e703fdcdb2581e767387c1d9
crashdemons <crashenator+gh@gmail.com>
2018-09-13 04:58:20 -0500

    document SkullConverter

--- src/main/java/com/github/crashdemons/playerheads/SkullConverter.java
+++ src/main/java/com/github/crashdemons/playerheads/SkullConverter.java
@@ -15,19 +15,31 @@ import org.bukkit.entity.EntityType;
 import org.shininet.bukkit.playerheads.LegacySkullType;
 
 /**
- * Converts between entities, custom skullState type, etc
- * @author crash
+ * Abstract class defining methods for converting between entities, custom skullState type, and legacy username-skulls, etc.
+ * @author crashdemons
  */
 public abstract class SkullConverter {
-    /*
     
-                    LegacySkullType customSkullType = LegacySkullType.valueOf(entityType.name());
-                    EntityDeathHelper(event, customSkullType, plugin.configFile.getDouble(customSkullType.name().replace("_", "").toLowerCase() + "droprate") * lootingrate);
-    */
+    /**
+     * Get the droprate config name (key) for the given skulltype.
+     * @param skullType the TexturedSkullType
+     * @return A string containing the config entry name (key) for the skulltype
+     * @see TexturedSkullType
+     */
     public static String dropConfigFromSkullType(TexturedSkullType skullType){
         if(skullType==TexturedSkullType.PLAYER) return "droprate";
         return skullType.name().replace("_", "").toLowerCase() + "droprate";
     }
+    
+    /**
+     * Convert an entity type to a TexturedSkullType.  Most mobs (and the player) have a 1:1 mapping.
+     * 
+     * Note: At the time of writing in 1.13.1 all entities with isAlive() have associated TexturedSkullType's with the exception of GIANT and ARMOR_STAND.
+     * Not all entities will have an associated skulltype, not all living entities will have an associated skulltype.
+     * 
+     * @param entityType The type of entity to get a skull for
+     * @return The associated TexturedSkullType, if one exists. Otherwise, null.
+     */
     public static TexturedSkullType skullTypeFromEntityType(EntityType entityType){
         String entityName = entityType.name().toUpperCase();
         try{
@@ -37,10 +49,29 @@ public abstract class SkullConverter {
         }
     }
     
+    /**
+     * Checks if the material provided is a player-head type of any description.
+     * 
+     * Checks whether the material is a Player Head or a Player Wall-Head.
+     * @param mat The material to check.
+     * @return true: the material is a playerhead. false: the material is not a playerhead.
+     */
     public static boolean isPlayerHead(Material mat){
         return (mat==Material.PLAYER_HEAD || mat==Material.PLAYER_WALL_HEAD);
     }
     
+    /**
+     * Attempts to determine a TexturedSkullType from a block's blockstate information.
+     * 
+     * This first attempts to guess the skulltype from the block's Material (whether it is a player or vanilla head drop).
+     * If it is a player-head, then it checks whether the associated UUID matches a TexturedSkullType entry.
+     * 
+     * 
+     * @param state The state associated with a block to determine the skulltype of.
+     * @return <ul><li>A TexturedSkullType associated with the mob (if material or UUID matched),</li>
+     *         <li>null (if the material is unsupported)</li>
+     *         <li>TexturedSkullType.PLAYER (if a playerhead UUID was not associated with any mob)</li></ul>
+     */
     public static TexturedSkullType skullTypeFromBlockState(BlockState state){
         TexturedSkullType type = TexturedSkullType.get(state.getType());//guess skullState by material
         if(type==null){
@@ -59,6 +90,21 @@ public abstract class SkullConverter {
         return match;//if match was not null
     }
     
+    /**
+     * Attempts to determine a TexturedSkullType from a block's blockstate information, with support for legacy username-based mobheads.
+     * 
+     * This method first checks skullTypeFromBlockState() for skulltype.
+     * If the skulltype returned is TexturedSkullType.PLAYER (unknown playerhead), 
+     * then this method attempts to associate the head's username with a LegacySkullType and upgrade it to the respective TexturedSkullType.
+     * 
+     * @param state The state associated with a block to determine the skulltype of.
+     * @return <ul><li>A TexturedSkullType associated with the mob (if material or UUID matched),</li>
+     *         <li>null (if the material is unsupported)</li>
+     *         <li>TexturedSkullType.PLAYER (if a playerhead UUID and username was not associated with any mob)</li></ul>
+     * @see #skullTypeFromBlockState(org.bukkit.block.BlockState) 
+     * @see org.shininet.bukkit.playerheads.LegacySkullType
+     * @deprecated Legacy username-based mobheads are supported for backwards-compatibility reasons but are deprecated in the long term, skullTypeFromBlockState should be used if legacy support is not needed.
+     */
     @Deprecated
     public static TexturedSkullType skullTypeFromBlockStateLegacy(BlockState state){//with legacy name matching support
         TexturedSkullType type = skullTypeFromBlockState(state);
@@ -80,6 +126,20 @@ public abstract class SkullConverter {
         
     }
     
+    /**
+     * Attempts to convert a LegacySkullType (username-based mobhead) to TexturedSkullType (texture-based mobhead).
+     * 
+     * Simple enum name conversion is used in this method.
+     * 
+     * Note: since 3.x changes making CustomSkullType (now LegacySkullType) uniform with bukkit's EntityType naming scheme, 
+     * all enum names in LegacySkullType should be present in TexturedSkullType.
+     * 
+     * @param oldType The LegacySkullType to convert
+     * @return The associated TexturedSkullType for the mobhead, or TexturedSkullType.PLAYER if no conversion was supported.
+     * @see org.shininet.bukkit.playerheads.LegacySkullType
+     * @see com.github.crashdemons.playerheads.TexturedSkullType
+     * @deprecated Legacy username-based mobheads are supported for backwards-compatibility reasons but are deprecated in the long term.
+     */
     @Deprecated
     public static TexturedSkullType upgradeSkullTypeLegacy(LegacySkullType oldType){
         try{
@@ -90,8 +150,17 @@ public abstract class SkullConverter {
         }
     }
     
-    
-    
+    /**
+     * Attempts to get a bukkit EntityType from the given TexturedSkullType.
+     * 
+     * Simple enum name conversion is used in this method - TexturedSkullType is intended to have all entries be in the same format as EntityType.
+     * Any entry in TexturedSkullType that doesn't exist in EntityType should be considered a bug or breaking changes in the bukkit API between major versions.
+     * 
+     * 
+     * 
+     * @param skullType The TexturedSkullType to find the associated entity-type for.
+     * @return The EntityType associated with the skulltype if one is found. Otherwise, in case of breaking API changes, returns null.
+     */
     public static EntityType entityTypeFromSkullType(TexturedSkullType skullType){
         String skullName = skullType.name().toUpperCase();
         try{

6576f0648d5510037f3f857d613a35dc9403ae55
crashdemons <crashenator+gh@gmail.com>
2018-09-13 04:10:17 -0500

    document package

---
+++ src/main/java/net/gravitydevelopment/updater/package-info.java
@@ -0,0 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Defines the BukkitDev update utility for plugins (not used by this branch)
+ * @author Gravity
+ * @see <a href="https://bukkit.org/threads/updater-2-3-easy-safe-and-policy-compliant-auto-updating-for-your-plugins-new.96681/">Updater thread</a>
+ */
+package net.gravitydevelopment.updater;

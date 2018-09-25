53ab2ab23aa86777d68025eac780b9b28b92e44f
crashdemons <crashenator+gh@gmail.com>
2018-09-24 21:16:25 -0500

    remove empty license templates, update license configuration to match license inherited from original code (MPL 2.0), advance version anticipating future changes.

--- src/main/java/org/shininet/bukkit/playerheads/package-info.java
+++ src/main/java/org/shininet/bukkit/playerheads/package-info.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 /**
  * Package containing code from or derived from the original PlayerHeads plugin

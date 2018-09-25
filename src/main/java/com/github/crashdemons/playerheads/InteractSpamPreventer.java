d5b49c447ba295661219bcd8a8658daa04129f62
crashdemons <crashenator+gh@gmail.com>
2018-09-24 21:38:04 -0500

    document spampreventer (non-functional change)

--- src/main/java/com/github/crashdemons/playerheads/InteractSpamPreventer.java
+++ src/main/java/com/github/crashdemons/playerheads/InteractSpamPreventer.java
@@ -7,7 +7,7 @@ import org.bukkit.block.Block;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 /**
- *
+ * Helper class that records and detects PlayerInteractEvent right-click spam for playerheads.
  * @author crash
  */
 public class InteractSpamPreventer {
@@ -38,12 +38,37 @@ public class InteractSpamPreventer {
             return false;
         }
     }
+    
+    /**
+     * Defines a spam-detection result object.
+     * 
+     * Used as semantic-sugar to make code self-documenting (eg:  `if(spampreventer.recordEvent(evt).isSpam())`  is more descriptive than `if(spampreventer.recordEvent(evt))`)
+     * 
+     */
     public class InteractSpamResult{
         private boolean spam;
+        /**
+         * <b><i>[INTERNAL - DO NOT USE]</i></b> Constructs a spam result
+         * @param spam the internal boolean state to abstract
+         */
         public InteractSpamResult(boolean spam){this.spam=spam;}
+        /**
+         * Reports whether the spam result indicates that the record was spam or not.
+         * 
+         * Reports the internal boolean state this class abstracts.
+         * @return true: it was detected as spam. false: it was not
+         */
         public boolean isSpam(){return spam;}
+        /**
+         * <b><i>[INTERNAL - DO NOT USE]</i></b> Toggles the internal boolean state the class abstracts.
+         */
         public void toggle(){ spam=!spam; }
     }
+    /**
+     * Records an interaction event internally and prepares a result after analyzing the event.
+     * @param event The PlayerInteractEvent to send to the spam-preventer.
+     * @return The Spam-detection Result object
+     */
     public synchronized InteractSpamResult recordEvent(PlayerInteractEvent event){
         InteractSpamResult result = new InteractSpamResult(false);
         InteractRecord record = new InteractRecord(event);

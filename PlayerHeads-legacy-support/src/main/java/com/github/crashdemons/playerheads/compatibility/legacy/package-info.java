86c0264dae58f04b02eaf6d5f6d1165c9f3b3b7f
crashdemons <crashenator+gh@gmail.com>
2019-04-28 02:28:08 -0500

    abstract legacy support class to reduce redundant code

---
+++ PlayerHeads-legacy-support/src/main/java/com/github/crashdemons/playerheads/compatibility/legacy/package-info.java
@@ -0,0 +1,10 @@
+/*
+ *  This Source Code Form is subject to the terms of the Mozilla Public
+ *  License, v. 2.0. If a copy of the MPL was not distributed with this
+ *  file, You can obtain one at http://mozilla.org/MPL/2.0/ .
+ */
+package com.github.crashdemons.playerheads.compatibility.legacy;
+
+/**
+ * Package providing operations common to all legacy (1.8-1.12.2) implementations
+ */

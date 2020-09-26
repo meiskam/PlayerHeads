a4744bcea8c00afd55c3aa568c644dca4e74794c
crashdemons <crashenator@gmail.com>
2020-09-26 16:34:59 -0500

    correct javadoc linking issue and rebuild docs [CI-SKIP]

---
+++ PlayerHeads-api/docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.github.crashdemons.playerheads.api"},{"l":"org.shininet.bukkit.playerheads"},{"l":"org.shininet.bukkit.playerheads.events"}]

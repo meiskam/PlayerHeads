57e9a5171bc8c2b1a08136759e7be3106d45c449
crashdemons <crashenator@gmail.com>
2021-06-06 11:37:06 -0500

    rebuild docs

--- PlayerHeads-api/docs/package-search-index.js
+++ PlayerHeads-api/docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.github.crashdemons.playerheads.api"},{"l":"org.shininet.bukkit.playerheads"},{"l":"org.shininet.bukkit.playerheads.events"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.github.crashdemons.playerheads.api"},{"l":"org.shininet.bukkit.playerheads"},{"l":"org.shininet.bukkit.playerheads.events"},{"l":"org.shininet.bukkit.playerheads.events.modifiers"}];updateSearchResults();
